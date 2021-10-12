53a330cf86d5a242c72cb929cc98feeadfd1a63b
Steve Jones <steve.jones763@gmail.com>
2021-10-12 11:58:45 +0100

    updated app.rb and index to use composite posts.

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require './lib/domain/post'
 require './lib/domain/comment'
+require './lib/domain/composite_post'
 
 class MessageBoardApp < Sinatra::Base
   get '/' do
@@ -17,6 +18,14 @@ class MessageBoardApp < Sinatra::Base
       Comment.new("This is the first comment on post 2", @posts[1].id, 456),
       Comment.new("This is the first comment on post 3", @posts[2].id, 456),
       Comment.new("This is the first comment on post 4", @posts[3].id, 456)
+    ]
+
+    @composite_posts = [
+      CompositePost.new(@posts[0], [@comments[0], @comments[1]]),
+      CompositePost.new(@posts[1], [@comments[2]]),
+      CompositePost.new(@posts[2], [@comments[3]]),
+      CompositePost.new(@posts[3], [@comments[4]]),
+      CompositePost.new(@posts[4], [])
     ]
     erb :index
   end
